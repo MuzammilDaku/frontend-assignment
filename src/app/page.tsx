c423649346243357b53229f1897fc358a57b78d5
MuzammilDaku <khanbalochmuzammilabbas@gmail.com>
2025-07-25 19:03:37 +0500

    Added Alert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+'use client';
+import { useEffect } from "react";
 import AddOocytes from "./components/AddOocytes";
 import Reports from "./components/Reports";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function App() {
+  useEffect(()=>{
+    alert("This is made by Muzammil Abbas \n Github Link:https://github.com/MuzammilDaku/frontend-assignment \n Mobile:0329247213")
+  },[])
   return (
    <div>
     <AddOocytes />
